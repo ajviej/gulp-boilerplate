9da25bbb7f9a619be725f81e0f9dd66e3bf2c0da
programer10 <programer10@ucione.local>
2019-06-10 20:17:08 +0200

    Fix errors in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,94 @@
-const 
+const
     //dev mode
-    devBuild= true,
+    devBuild = true,
 
     //modules
-    gulp=require('gulp'),
-    newer=require('gulp-newer'),
-    noop=require('gulp-noop'),
-    htmlclean=require('gulp-htmlclean'),
-    sync= require('browser-sync').create(),
-    sass =require('gulp-sass'),
+    gulp = require('gulp'),
+    newer = require('gulp-newer'),
+    noop = require('gulp-noop'),
+    htmlclean = require('gulp-htmlclean'),
+    sync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     terser = require('gulp-terser'),
     //paths
-    src='/src',
-    build='/build';
+    src = 'src',
+    build = 'build';
 
-    // HTML processing
-    function html(){
-        return gulp.src(src+ 'html/**/*')
-            .pipe(newer(build))
-            .pipe(devBuild? noop() :htmlclean())
-            .pipe(gulp.dest(build))
-            .pipe(sync.stream());
-    }
+// HTML processing
+function html() {
+    return gulp.src(src + 'html/**/*')
+        .pipe(newer(build))
+        .pipe(devBuild ? noop() : htmlclean())
+        .pipe(gulp.dest(build))
+        .pipe(sync.stream());
+}
 
-    
-    // CSS processing
-    function css(){
-        const out= build + '/assets/css'
 
-        return gulp.src(src+'scss/main.scss')
-            .pipe(sass({
-                errorLogToConsole: devBuild,
-                outputStyle:devBuild ? 'expanded' :'compressed'
-            })).on('error', sass.logError)
-            .pipe(gulp.dest(out))
-            .pipe(sync.stream());
-    }
+// CSS processing
+function css() {
+    const out = build + '/assets/css'
 
-    
-    // JavaScript processing
-    function js(){
-        const out= build + '/assets/js'
-
-        return gulp.src([
-            src + '/js/**/*'
-
-        ])
-        .pipe(newer(out))
-        .pipe(concat('bundle.min.js'))
-        .pipe(devBuild? noop() :terser())//minimizira
+    return gulp.src(src + '/scss/main.scss')
+        .pipe(sass({
+            errorLogToConsole: devBuild,
+            outputStyle: devBuild ? 'expanded' : 'compressed'
+        })).on('error', sass.logError)
         .pipe(gulp.dest(out))
         .pipe(sync.stream());
-    }
+}
 
-    //watch for file changes
+
+// JavaScript processing
+function js() {
+    const out = build + '/assets/js'
+
+    return gulp.src([
+        src + '/js/**/*'
+
+    ])
+        .pipe(newer(out))
+        .pipe(concat('bundle.min.js'))
+        .pipe(devBuild ? noop() : terser())//minimizira
+        .pipe(gulp.dest(out))
+        .pipe(sync.stream());
+}
+
+//watch for file changes
 /**
  * Watch for file changes
  * @param {*} done 
  * @return void  //ako nemamo neki return funkcija vraca void
  */
-    function watch(done){
-        sync.init({
-            server:{
-                baseDir: '.'+build
-            }
-        });
+function watch(done) {
+    sync.init({
+        server: {
+            baseDir: './' + build
+        }
+    });
 
-        //html changes
-        gulp.watch(src + '/html/**/*', html);
+    //html changes
+    gulp.watch(src + '/html/**/*', html);
 
-        //CSS changes
-        gulp.watch(src + '/scss/**/*', scss);
+    //CSS changes
+    gulp.watch(src + '/scss/**/*', css);
 
-        //JS changes
-        gulp.watch(src + '/js/**/*', js);
-        
-        sync.reload();
+    //JS changes
+    gulp.watch(src + '/js/**/*', js);
 
-        done();
+    sync.reload();
 
-    }
+    done();
 
-    // create gulp single tasks
+}
+
+// create gulp single tasks
 exports.html = html;
 exports.js = js;
 exports.css = css;
 exports.watch = watch;
 
 // run all tasks
-exports.build = gulp.parallel(exports.html, exports.js);
+exports.build = gulp.parallel(exports.html,exports.css,  exports.js);
 // default task
 exports.default = gulp.series(exports.build, exports.watch);
